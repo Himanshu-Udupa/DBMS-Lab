c05615b86c718271fa1a80462a57c4a41a78f83b
Himanshu Udupa G <115914825+Himanshu-Udupa@users.noreply.github.com>
2024-01-12 06:38:31 +0530

    Update sailors.sql

--- sailors.sql
+++ sailors.sql
@@ -1,119 +1,97 @@
-create database sailors;
-use sailors;
+-- sailors database
+CREATE DATABASE SAILORS;
+USE SAILORS;
 
-create table Sailors(
-	sid int primary key,
-	sname varchar(35),
-	rating float,
-	age int
+CREATE TABLE SAILOR(
+sid INT PRIMARY KEY,
+sname VARCHAR(30),
+rating FLOAT,
+age INT
 );
 
-create table Boat(
-	bid int primary key,
-	bname varchar(35),
-	color varchar(25)
+CREATE TABLE BOAT(
+bid INT PRIMARY KEY,
+bname VARCHAR(15),
+color TEXT
 );
 
-create table reserves(
-	sid int,
-	bid int,
-	sdate date,
-	foreign key (sid) references Sailors(sid) on delete cascade,
-	foreign key (bid) references Boat(bid) on delete cascade
+CREATE TABLE RESERVES(
+sid INT,
+bid INT,
+date DATE,
+FOREIGN KEY (sid) REFERENCES SAILOR(sid) ON DELETE CASCADE,
+FOREIGN KEY (bid) REFERENCES BOAT(bid) ON DELETE CASCADE
 );
 
-insert into Sailors values
-(1,"Albert", 5.0, 40),
-(2, "Nakul", 5.0, 49),
-(3, "Darshan", 9, 18),
-(4, "Astorm Gowda", 2, 68),
-(5, "Armstormin", 7, 19);
+INSERT INTO SAILOR VALUES
+(1,"Albert",5.0,45),
+(2,"Nakul",6.0,18),
+(3,"Sandeep",5.0,42),
+(4,"Syed Stormer",9.0,30),
+(5,"Neil Armstorm",8.4,28);
 
+INSERT INTO BOAT VALUES
+(101,"Boat 1","Green"),
+(102,"Boat 2","Red"),
+(103,"Boat 3","Yellow");
 
-insert into Boat values
-(1,"Boat_1", "Green"),
-(2,"Boat_2", "Red"),
-(103,"Boat_3", "Blue");
+INSERT INTO RESERVES VALUES
+(1,101,"2024-01-12"),
+(2,102,"2024-01-20"),
+(1,102,"2024-01-24"),
+(3,103,"2024-02-01"),
+(1,103,"2024-01-15"),
+(2,101,"2024-01-18");
 
-insert into reserves values
-(1,103,"2023-01-01"),
-(1,2,"2023-02-01"),
-(2,1,"2023-02-05"),
-(3,2,"2023-03-06"),
-(5,103,"2023-03-06"),
-(1,1,"2023-03-06");
-
-select * from Sailors;
-select * from Boat;
-select * from reserves;
+SELECT * FROM SAILOR;
+SELECT * FROM BOAT;
+SELECT * FROM RESERVES;
 
 -- Find the colours of the boats reserved by Albert
-select color 
-from Sailors s, Boat b, reserves r 
-where s.sid=r.sid and b.bid=r.bid and s.sname="Albert";
+SELECT color FROM SAILOR s, BOAT b, RESERVES r WHERE s.sid=r.sid AND b.bid=r.bid AND s.sname = "Albert";
+
 
 -- Find all the sailor sids who have rating atleast 8 or reserved boat 103
-(select sid
-from Sailors
-where Sailors.rating>=8)
-UNION
-(select sid
-from reserves
-where reserves.bid=103);
+SELECT sid FROM SAILOR WHERE rating>=8 UNION SELECT sid FROM RESERVES WHERE bid=103;
 
 
 -- Find the names of the sailor who have not reserved a boat whose name contains the string "storm". Order the name in the ascending order
-select s.sname
-from Sailors s
-where s.sid not in 
-(select s1.sid from Sailors s1, reserves r1 where r1.sid=s1.sid and s1.sname like "%storm%")
-and s.sname like "%storm%"
-order by s.sname ASC;
+SELECT s.sname FROM SAILOR s where s.sid NOT IN(SELECT s1.sid FROM SAILOR s1, RESERVES r WHERE s1.sid=r.sid AND s1.sname LIKE "%storm%") AND s.sname LIKE "%storm%" ORDER BY s.sname;
+
 
 -- Find the name of the sailors who have reserved all boats
-select sname from Sailors s where not exists
-	(select * from Boat b where not exists
-		(select * from reserves r where r.sid=s.sid and b.bid=r.bid));
+SELECT sname FROM SAILOR s WHERE NOT EXISTS( SELECT * FROM BOAT b WHERE NOT EXISTS(SELECT * FROM RESERVES r WHERE s.sid=r.sid AND r.bid=b.bid));;
 
 
 -- Find the name and age of the oldest sailor
-select sname, age
-from Sailors where age in (select max(age) from Sailors);
+SELECT sname, age FROM SAILOR WHERE age IN(SELECT MAX(age) FROM SAILOR);
+
 
 -- For each boat which was reserved by atleast 2 sailors with age >= 40, find the bid and average age of such sailors
-select b.bid, avg(s.age) as average_age
-from Sailors s, Boat b, reserves r
-where r.sid=s.sid and r.bid=b.bid and s.age>=40
-group by bid
-having 2<=count(distinct r.sid);
+SELECT b.bid, AVG(s.age) AS average_age FROM SAILOR s, BOAT b, RESERVES r WHERE s.sid=r.sid AND r.bid=b.bid AND s.age>=40 GROUP BY bid HAVING 2<=COUNT(DISTINCT r.sid);
 
--- A view that shows names and ratings of all sailors sorted by rating in descending order
-create view NamesAndRating as
-select sname, rating
-from Sailors
-order by rating DESC;
-
-select * from NamesAndRating;
 
 -- Create a view that shows the names and colours of all the boats that have been reserved by a sailor with a specific rating.
-create view ReservedBoatsWithRatedSailor as
-select distinct bname, color
-from Sailors s, Boat b, reserves r
-where s.sid=r.sid and b.bid=r.bid and s.rating=5;
+CREATE VIEW NameAndColor AS
+SELECT DISTINCT bname, color
+FROM SAILOR s, BOAT b, RESERVES r
+WHERE s.sid=r.sid AND r.bid=b.bid
+AND s.rating=5;
 
-select * from ReservedBoatsWithRatedSailor;
+SELECT * FROM NameAndColor;
+
 
 -- Trigger that prevents boats from being deleted if they have active reservation
 DELIMITER //
-create or replace trigger CheckAndDelete
-before delete on Boat
+CREATE TRIGGER PreventDelete
+before delete on BOAT
 for each row
 BEGIN
-	IF EXISTS (select * from reserves where reserves.bid=old.bid) THEN
-		SIGNAL SQLSTATE '45000' SET message_text='Boat is reserved and hence cannot be deleted';
-	END IF;
+       IF EXISTS (SELECT * FROM RESERVES WHERE RESERVES.bid=old.bid) THEN
+           SIGNAL SQLSTATE '45000' SET message_text="Boat Has active reservation hence cannot delete";
+       END IF;
 END;//
 
 DELIMITER ;
 
-delete from Boat where bid=103; -- This gives error since boat 103 is reserved
+DELETE FROM BOAT WHERE bid=103; -- This gives error since boat 103 is reserved
